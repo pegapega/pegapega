16dd078ab72ee88c68638f849416455aa01171e9
Armando Neto <neto.armando@gmail.com>
2013-01-30 21:55:35 -0200

    deacoplamento

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('',
-    url(r'^login/$', 'pegapega.apps.accounts.views.login', name='facebook_login'),
-    url(r'^authentication_callback/$', 'pegapega.apps.accounts.views.callback', name='facebook_callback'),
+    url(r'^login/$', 'accounts.views.login', name='facebook_login'),
+    url(r'^authentication_callback/$', 'accounts.views.callback', name='facebook_callback'),
     url(r'^logout/$', 'django.contrib.auth.views.logout'),
 )
