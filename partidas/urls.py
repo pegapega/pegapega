cb9ed344a6a47ab24dcb7a666bf50c390e93bd42
Sergio Oliveira <sergio@tracy.com.br>
2013-01-30 19:43:54 -0200

    Renomeando template

--- partidas/urls.py
+++ partidas/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import patterns, include, url
 from django.views.generic.simple import direct_to_template
 
 urlpatterns = patterns('',
-    url(r'^$', direct_to_template, {'template': 'partidas/partidas.html'}),
+    url(r'^$', direct_to_template, {'template': 'partidas/list.html'}),
 )
