8ae043ea348453b4f707fc19663061db2433c777
Armando Neto <neto.armando@gmail.com>
2013-01-31 01:16:47 -0200

    teste de camera + upload pra timeline do facebook [BUG]: ffos simulator crasha quando abro app

--- partidas/urls.py
+++ partidas/urls.py
@@ -3,4 +3,6 @@ from django.views.generic.simple import direct_to_template
 
 urlpatterns = patterns('',
     url(r'^$', direct_to_template, {'template': 'partidas/list.html'}),
+    url(r'^partida/new$', direct_to_template, {'template': 'partidas/nova_partida.html'}),
+    url(r'^test/$', 'partidas.views.test', name='test'),
 )
--- partidas/views.py
+++ partidas/views.py
@@ -1,1 +1,24 @@
-# Create your views here.
+from django.shortcuts import render_to_response, get_object_or_404
+from django.template import RequestContext
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+
+from accounts.models import UserProfile
+
+import requests
+
+@login_required
+def test(request):
+
+    fb_user = UserProfile.objects.get(user=request.user)
+
+    r = requests.get('https://graph.facebook.com/me/photos', params={
+            'access_token': fb_user.access_token
+        })
+
+    user_accounts = r.text
+    print user_accounts
+
+    return render_to_response('partidas/test.html', {
+        'facebook_id': fb_user.facebook_id,
+    }, context_instance=RequestContext(request))
