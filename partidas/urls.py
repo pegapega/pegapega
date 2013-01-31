5abd90a3bf41e3453e1c182aafafcf15165c954b
Armando Neto <neto.armando@gmail.com>
2013-01-31 11:44:14 -0200

    mudando pra camera

--- partidas/urls.py
+++ partidas/urls.py
@@ -4,5 +4,5 @@ from django.views.generic.simple import direct_to_template
 urlpatterns = patterns('',
     url(r'^$', direct_to_template, {'template': 'partidas/list.html'}),
     url(r'^partida/new$', direct_to_template, {'template': 'partidas/nova_partida.html'}),
-    url(r'^test/$', 'partidas.views.test', name='test'),
+    url(r'^test/$', 'partidas.views.take_photo', name='take_photo'),
 )
--- partidas/views.py
+++ partidas/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render_to_response, get_object_or_404
+from django.shortcuts import render_to_response, redirect, get_object_or_404
 from django.template import RequestContext
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
@@ -8,17 +8,44 @@ from accounts.models import UserProfile
 import requests
 
 @login_required
-def test(request):
+def take_photo(request):
 
     fb_user = UserProfile.objects.get(user=request.user)
 
-    r = requests.get('https://graph.facebook.com/me/photos', params={
-            'access_token': fb_user.access_token
-        })
+    if request.method == 'POST':
+        files = {'source': open('/Users/neto/Pictures/xx.jpg', 'r')}
 
-    user_accounts = r.text
-    print user_accounts
 
-    return render_to_response('partidas/test.html', {
-        'facebook_id': fb_user.facebook_id,
-    }, context_instance=RequestContext(request))
+        r = requests.post('https://graph.facebook.com/me/photos', params={
+                'access_token': fb_user.access_token
+            }, files=files)
+
+
+        print r
+        return 'ok'
+
+
+    else:
+
+        files = {'source': open('/Users/neto/Pictures/xx.jpg', 'r')}
+
+
+        r = requests.post('https://graph.facebook.com/me/photos', params={
+                'access_token': fb_user.access_token
+            }, files=files)
+
+
+        print r
+        return 'ok'
+
+        # r = requests.get('https://graph.facebook.com/me/photos', params={
+        #         'access_token': fb_user.access_token
+        #     })
+
+        # user_accounts = r.text
+        # print user_accounts
+
+        return render_to_response('partidas/camera.html', {
+            # 'facebook_id': fb_user.facebook_id,
+        }, context_instance=RequestContext(request))
+
