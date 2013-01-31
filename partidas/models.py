1c7fa1d3aff29c924e795052014867d13b15dc4f
Sergio Oliveira <sergio@tracy.com.br>
2013-01-30 22:06:30 -0200

    Configurando admin para partidas

--- partidas/models.py
+++ partidas/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Partida(models.Model):
     nome = models.CharField(max_length=30)
     num_players = models.IntegerField()
 
 class JogandoPartida(models.Model):
-    user = models.OneToOneField('auth.User')
+    user = models.OneToOneField(User)
     partida = models.ForeignKey(Partida)
