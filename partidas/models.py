a488078264181e16a467d19e030331b7141e2fc2
Sergio Oliveira <sergio@tracy.com.br>
2013-02-01 03:53:09 -0200

    Adiciionando codigo da partida

--- partidas/models.py
+++ partidas/models.py
@@ -7,9 +7,25 @@ class Partida(models.Model):
     def __unicode__(self):
         return self.nome
 
+
 class JogandoPartida(models.Model):
     user = models.ForeignKey(User)
     partida = models.ForeignKey(Partida)
+    vivo = models.BooleanField(default=True)
+    codigo_partida = models.IntegerField(default=-1)
 
     def __unicode__(self):
         return '{1}: {0}'.format(self.user.get_full_name(), self.partida.nome)
+
+    def save(self, *args, **kwargs):
+        if self.codigo_partida == -1:
+            partida = JogandoPartida.objects.filter(partida=self.partida)
+            try:
+                partida = partida.order_by("-codigo_partida")[0]
+                max_codigo = partida.codigo_partida
+            except JogandoPartida.DoesNotExist:
+                max_codigo = -1
+
+            self.codigo_partida = max_codigo + 1
+        super(JogandoPartida, self).save(*args, **kwargs)
+
